9861aa484fc2889f2b8d394a69af07a7a8f4179d
Nadi <kitti-46@ya.ru>
2023-11-16 00:47:14 +0300

    added 5Home

---
+++ 5Home/1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*
+Дан объект numbers. 
+Необходимо в консоль вывести все значения объекта, которые больше или равны 3.
+*/
+
+const numbers = {
+  key1: 12,
+  key2: 2,
+  key3: 4,
+  key4: 1,
+  key5: -244,
+  key6: 0,
+  key7: 7,
+};
+
+const numbs = Object.values(numbers);
+const newNumbers = numbs.filter((numb) => numb >= 3);
+console.log(newNumbers);
